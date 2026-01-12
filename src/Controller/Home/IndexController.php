75e4811cdfcc551d37de9a4afb584891fd49dcf5
darkirby <darkirby.tuta.com>
2026-01-12 21:07:30 +0100

    Add bootstrap and layout basics

---
+++ src/Controller/Home/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Home;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('', name: 'home_')]
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', []);
+    }
+}
---
+++ src/Controller/Necesse/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller\Necesse;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/necesse', name: 'necesse_')]
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('necesse/index.html.twig', []);
+    }
+}
