c6f32b399d35aa9936e20b5dc45b3e2f04002587
darkirby <darkirby@tuta.com>
2026-05-20 20:39:35 +0200

    Add handler to redirect when authenticated but forbidden

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function __construct(#[Autowire('%app.hub_url%')] private string $hubUrl) {}
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
+    {
+        return new RedirectResponse($this->hubUrl);
+    }
+}
